4e39bb997d9f89a62a8cb9487caf8b3ca562ae8b
Alisson Gutierrez <vaneg643@gmail.com>
2020-07-19 18:32:53 -0400

    add cors

--- src/projects/__init__.py
+++ src/projects/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_marshmallow import Marshmallow
 from flask_bcrypt import Bcrypt
+from flask_cors import CORS
 from projects.configs import Config
 import marshmallow
 
@@ -27,6 +28,7 @@ def register_error_handler(app):
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
     app.config.from_object(Config)
 
     db.init_app(app)
